ddc55525f52499753226575a42bd8572d94afa0d
Jordi <albiol.jordi@gmail.com>
2026-05-24 21:41:37 +0200

    setup quantum optimization and tensor network environment

--- app/quantum/expectation.py
+++ app/quantum/expectation.py
@@ -1,4 +1,25 @@
-from qiskit.quantum_info import Statevector
+# from qiskit.quantum_info import Statevector
+
+
+# def compute_expectation_value(
+#     circuit,
+#     hamiltonian
+# ):
+
+#     state = Statevector.from_instruction(
+#         circuit
+#     )
+
+#     energy = state.expectation_value(
+#         hamiltonian
+#     )
+
+#     return energy.real
+
+
+from app.quantum.statevector import (
+    get_statevector
+)
 
 
 def compute_expectation_value(
@@ -6,9 +27,7 @@ def compute_expectation_value(
     hamiltonian
 ):
 
-    state = Statevector.from_instruction(
-        circuit
-    )
+    state = get_statevector(circuit)
 
     energy = state.expectation_value(
         hamiltonian
